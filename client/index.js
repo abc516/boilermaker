5aa13f94ae35193b789d74ab06a9f1a9d7dad783
Anton Cataldi <antoncataldi@gmail.com>
2017-08-09 16:08:59 -0400

    add: react and redux boilerplate code

--- client/index.js
+++ client/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
+import store from 'store'
 
 ReactDOM.render(
-  <div>Hello world!</div>,
+  <Provider store={store}>
+    {/* stuff goes here */}
+  </Provider>,
   document.getElementById('app')
 );
